0ea678549aee07604e1721fc9eaf6c8efa97ec50
Ryan Le <le_ryan@hotmail.com>
2025-02-09 19:08:33 -0600

    feat: added google maps

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -15,33 +15,33 @@ function Header() {
   return (
     <div className="p-6 px-10 flex justify-between shadow-sm fixed top-0 w-full z-10 bg-white">
       <div className="flex gap-10 items-center">
-        <Link href="/">
-          <Image src={"/logo.svg"} width={150} height={150} alt="logo" />
+        <Link href="/" onClick={() => window.location.reload()}>
+          <Image src={"/logo.svg"} width={100} height={100} alt="logo" />
         </Link>
         <ul className="hidden md:flex gap-10 items-center">
           <Link href={"/"}>
             <li
-              className={`hover:text-primary font-medium text-sm cursor-pointer ${
+              className={`hover:text-primary font-medium text-xl cursor-pointer ${
                 path == "/" && "text-primary"
               }`}
             >
               For Sell
             </li>
           </Link>
-          <Link href={"/"}>
+          {/* <Link href={"/"}>
             <li
-              className={`hover:text-primary font-medium text-sm cursor-pointer`}
+              className={`hover:text-primary font-medium text-xl cursor-pointer`}
             >
               For Rent
             </li>
           </Link>
           <Link href={"/"}>
             <li
-              className={`hover:text-primary font-medium text-sm cursor-pointer`}
+              className={`hover:text-primary font-medium text-xl cursor-pointer`}
             >
               Agent Finder
             </li>
-          </Link>
+          </Link> */}
         </ul>
       </div>
       <div className="flex gap-2 items-center">
