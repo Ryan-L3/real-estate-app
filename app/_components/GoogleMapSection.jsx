957a6a7775496b8fa2b431225da31d2432895e2a
Ryan Le <le_ryan@hotmail.com>
2025-02-09 22:06:03 -0600

    fix: updated map styling

--- app/_components/GoogleMapSection.jsx
+++ app/_components/GoogleMapSection.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-import { useLoadScript } from "@react-google-maps/api";
-import dynamic from "next/dynamic";
-
-const GoogleMap = dynamic(
-  () => import("@react-google-maps/api").then((mod) => mod.GoogleMap),
-  { ssr: false }
-);
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "100%",
   height: "80vh",
+  borderRadius: 10,
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523,
+  lat: 49.8954,
+  lng: -97.1385,
 };
 
+const libraries = ["places"];
+
 function GoogleMapSection() {
-  const { isLoaded } = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY,
+    libraries,
   });
 
+  if (loadError) return <div>Error loading maps</div>;
   if (!isLoaded) return <div>Loading...</div>;
 
   return (
